5c2e5227951590eb80425c9adcfc911ae290c137
MnlPhlp <online@philipp-manuel.de>
2025-04-06 04:01:36 +0200

    update site generation

--- site/generate.sh
+++ site/generate.sh
@@ -2,10 +2,11 @@
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SITE_DIR=$(realpath "$SCRIPT_DIR/../_site")
 cd "$SCRIPT_DIR"
 
 list=""
-
+mkdir -p "$SITE_DIR"
 for dir in ../games/*; do
     name=$(basename "$dir")
     dir=$(realpath "$dir")
@@ -13,12 +14,12 @@ for dir in ../games/*; do
     cd "$dir"
     cargo build --release --target wasm32-unknown-unknown
     list+="        <li><a href=\"$name.html\">$name</a></li>\n"
-    cd "$SCRIPT_DIR"
+    cd "$SITE_DIR"
     cp "../target/wasm32-unknown-unknown/release/$name.wasm" "$name.wasm"
-    cp game.html.template "$name.html"
+    cp "$SCRIPT_DIR/game.html.template" "$name.html"
     sed -i "s|GAME_NAME|$name|" "$name.html"
 done
 
 echo "Generating index.html"
-cp index.html.template index.html
+cp "$SCRIPT_DIR/index.html.template" index.html
 sed -i "s|LIST_ITEMS|$list|" index.html
